752038afc287049b121bba78aab25b26facd3d07
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-08 23:52:39 -0800

    updated test to actually expect an integer rather than a float

--- tests.rb
+++ tests.rb
@@ -7,6 +7,6 @@ end
 parse_test = Parse.new('data.txt')
 assert(parse_test.instance_of?(Parse))
 assert(parse_test.get_string('meta data', 'correction text') == "I meant 'moderately,' not 'tediously,' above.")
-assert(parse_test.get_integer('header', 'budget') == 4.5)
+assert(parse_test.get_integer('header', 'budget') == 4)
 
 puts "And we're green; all tests have passed!"
