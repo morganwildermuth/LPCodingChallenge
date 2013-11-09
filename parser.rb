11fb86158d6d9bce9ad3c951071d7444c24077ce
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-08 21:04:16 -0800

    add empty methods for what will ult. be needed to Parse class

--- parser.rb
+++ parser.rb
@@ -1,5 +1,23 @@
 class Parse
   def initialize(file)
   end
+
+  def get_string(section, key)
+  end
+
+  def get_integer(section, key)
+  end
+
+  def get_floating_point(section, key)
+  end
+
+  def set_string(section, key)
+  end
+
+  def set_integer(section, key)
+  end
+
+  def set_floating_point(section, key)
+  end
 end
 
--- spec/tests.rb
+++ spec/tests.rb
@@ -4,6 +4,7 @@ def assert(arg)
   raise 'and we got a failure' unless arg
 end
 
-assert(Parse.new('data.rb').instance_of?(Parse))
+parse_test = Parse.new('data.rb')
+assert(parse_test.instance_of?(Parse))
 
 puts "And we're green; all tests have passed!"
