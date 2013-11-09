f81eaeb5db108260ba814baf5562bcc955e9adde
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-09 00:08:27 -0800

    refactoring via attr_reader; functionality same

--- parser.rb
+++ parser.rb
@@ -1,21 +1,21 @@
 class Parse
-  attr_reader :file, :file_hash
+  attr_reader :file, :file_by_line_array, :file_hash
 
   def initialize(file)
     @file = file
+    @file_hash = {}
+    @file_by_line_array = IO.readlines(file)
     file_to_hash
   end
 
   def file_to_hash
-    @file_hash = {}
-    f = File.new(file)
     line_trimmed = 'empty'
     current_key = 'empty'
     file_by_line_array.each do |line|
       if line[0] == '['
         line_trimmed = line.gsub!(/\[|\]/, "")
         line_trimmed = line_trimmed.strip
-        @file_hash[line_trimmed] = {}
+        file_hash[line_trimmed] = {}
       else
         unless line[0] == "\n" 
           line = line.delete("\n")
@@ -24,11 +24,11 @@ class Parse
               key_value_pair = line.split(':')
               key = key_value_pair[0].strip
               value = key_value_pair[1].strip
-              @file_hash[line_trimmed][key] = value
+              file_hash[line_trimmed][key] = value
               current_key = key
             end
           else
-            @file_hash[line_trimmed][current_key] = @file_hash[line_trimmed][current_key] + line
+            file_hash[line_trimmed][current_key] = file_hash[line_trimmed][current_key] + line
           end
         end
       end
