b54dd40a05c0eed15df010f92b1817fec39a1139
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-11 03:26:29 -0800

    add tests for resetting data to original format

--- tests.rb
+++ tests.rb
@@ -4,16 +4,35 @@ def assert(arg)
   raise 'and we got a failure' unless arg
 end
 
-parse_test = Parse.new('data.txt')
+def reset_test_data(file)
+  test_file = file 
+  file_by_line_array = File.readlines(test_file)
+  File.open(test_file, "w") do |file|
+  file_by_line_array.each { |line| 
+    file.puts(line) unless line.include?("priorities") || line.include?("mindset") }
+  end
+end
+
+test_file = 'data.txt'
+
+# tests for getting
+parse_test = Parse.new(test_file)
 
 assert(parse_test.instance_of?(Parse))
 assert(parse_test.get_string('meta data', 'correction text') == "I meant 'moderately,' not 'tediously,' above.")
 assert(parse_test.get_integer('header', 'budget') == 4)
 assert(parse_test.get_float('header', 'budget') == 4.5)
 
+#tests for setting
 parse_test.set_string('priorities', 'mindset', 'constant learner')
+parse_test = Parse.new(test_file)
 
-assert(parse_test.get_string('top priority', 'mindset') == 'constant learner')
+assert(parse_test.get_string('priorities', 'mindset') == 'constant learner')
 
+#tests for resetting test data
+reset_test_data(test_file)
+parse_test = Parse.new(test_file)
+
+assert(parse_test.get_string('priorities', 'mindset') == 'No such item.')
 
 puts "And we're green; all tests have passed!"
