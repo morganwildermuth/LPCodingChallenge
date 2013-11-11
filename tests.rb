fb54ce47863d8934c26bbf6b3d68515e127f46f2
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-11 02:31:49 -0800

    failing tests for set_string

--- tests.rb
+++ tests.rb
@@ -5,9 +5,15 @@ def assert(arg)
 end
 
 parse_test = Parse.new('data.txt')
+
 assert(parse_test.instance_of?(Parse))
 assert(parse_test.get_string('meta data', 'correction text') == "I meant 'moderately,' not 'tediously,' above.")
 assert(parse_test.get_integer('header', 'budget') == 4)
 assert(parse_test.get_float('header', 'budget') == 4.5)
+
+parse_text.set_string('priorities', 'mindset', 'constant learner')
+
+assert(parse_test.get_string('top priority', 'mindset') == 'constant learner')
+
 
 puts "And we're green; all tests have passed!"
