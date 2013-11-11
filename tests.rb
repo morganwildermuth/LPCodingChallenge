d9d565668f793ffb67cc15c7d164ca5fcbf1b4c5
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-11 03:52:36 -0800

    added tests for adding floats and integers to test data

--- tests.rb
+++ tests.rb
@@ -23,10 +23,13 @@ assert(parse_test.get_integer('header', 'budget') == 4)
 assert(parse_test.get_float('header', 'budget') == 4.5)
 
 #tests for setting
-parse_test.set_string('priorities', 'mindset', 'constant learner')
+parse_test.add_value('priorities', 'mindset', 'constant learner')
+parse_test.add_value('extra priorities data', 'mindset degree of awesome', '10.01')
 parse_test = Parse.new(test_file)
 
 assert(parse_test.get_string('priorities', 'mindset') == 'constant learner')
+assert(parse_test.get_integer('extra priorities data', 'mindset degree of awesome') == 10)
+assert(parse_test.get_float('extra priorities data', 'mindset degree of awesome') == 10.01)
 
 #tests for resetting test data
 reset_test_data(test_file)
