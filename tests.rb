99680fbd8f3bea9d7e15cc7f45699f1a1c5e9d2d
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-11 17:35:06 -0800

    cleaned up tests page

--- tests.rb
+++ tests.rb
@@ -1,35 +1,22 @@
 require_relative "parser.rb"
-
-def assert(arg)
-  raise 'and we got a failure' unless arg
-end
-
-def reset_test_data(file)
-  test_file = file 
-  file_by_line_array = File.readlines(test_file)
-  File.open(test_file, "w") do |file|
-  file_by_line_array.each { |line| 
-    file.puts(line) unless line.include?("priorities") || line.include?("mindset") || line.include?("attitude") }
-  end
-end
+require_relative "test_methods.rb"
 
 test_file = 'data.txt'
 
-# tests for getting
+# tests for getting initial values
 parse_test = Parse.new(test_file)
 
 assert(parse_test.get_string('meta data', 'correction text') == "I meant 'moderately,' not 'tediously,' above.")
 assert(parse_test.get_integer('header', 'budget') == 4)
 assert(parse_test.get_float('header', 'budget') == 4.5)
 
-#tests for setting
+#tests for setting and retrieving added values
 parse_test.add_value('priorities', 'mindset', 'constant learner')
 parse_test.add_value('priorities', 'attitude', 'can do')
 parse_test.add_value('extra priorities data', 'mindset degree of awesome', '10.01')
-parse_test = Parse.new(test_file)
 
 assert(parse_test.get_string('priorities', 'mindset') == 'constant learner')
-# assert(parse_test.get_string('priorities', 'attitude') == 'can do')
+assert(parse_test.get_string('priorities', 'attitude') == 'can do')
 assert(parse_test.get_integer('extra priorities data', 'mindset degree of awesome') == 10)
 assert(parse_test.get_float('extra priorities data', 'mindset degree of awesome') == 10.01)
 
