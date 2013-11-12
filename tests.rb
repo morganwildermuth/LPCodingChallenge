cc6bb9da32199340ac995edefbee65adfaeb91c4
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-11 16:31:13 -0800

    add failing test for adding another key to an already existing section

--- tests.rb
+++ tests.rb
@@ -24,10 +24,12 @@ assert(parse_test.get_float('header', 'budget') == 4.5)
 
 #tests for setting
 parse_test.add_value('priorities', 'mindset', 'constant learner')
+parse_test.add_value('priorities', 'attitude', 'can do')
 parse_test.add_value('extra priorities data', 'mindset degree of awesome', '10.01')
 parse_test = Parse.new(test_file)
 
 assert(parse_test.get_string('priorities', 'mindset') == 'constant learner')
+assert(parse_test.get_string('priorities', 'attitude') == 'can do')
 assert(parse_test.get_integer('extra priorities data', 'mindset degree of awesome') == 10)
 assert(parse_test.get_float('extra priorities data', 'mindset degree of awesome') == 10.01)
 
