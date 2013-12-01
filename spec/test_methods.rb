d410e335f38bd718f68ef22a3dc09718221606f4
morganwildermuth <morgan.wildermuth@gmail.com>
2013-11-30 23:11:17 -0800

    in reset data used do/end block instead of {}

--- spec/test_methods.rb
+++ spec/test_methods.rb
@@ -2,7 +2,8 @@ def reset_test_data(file)
   test_file = file 
   file_by_line_array = File.readlines(test_file)
   File.open(test_file, "w") do |file|
-  file_by_line_array.each { |line| 
-    file.puts(line) unless line.include?("priorities") || line.include?("mindset") || line.include?("attitude") }
+    file_by_line_array.each do |line|
+      file.puts(line) unless line.include?("priorities") || line.include?("mindset") || line.include?("attitude")
+    end
   end
 end
